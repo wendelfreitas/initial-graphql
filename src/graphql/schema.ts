2a86d1bd789fafbc29c810d2d25d73dbcf26d544
Wendel Freitas <wendel.freitas@scaffoldeducation.com.br>
2019-03-08 11:35:18 -0300

    Initial commit :rocket:

---
+++ src/graphql/schema.ts
@@ -0,0 +1,42 @@
+import { makeExecutableSchema } from "graphql-tools";
+
+const users: any[] = [
+  {
+    id: 1,
+    name: "John",
+    email: "John@6"
+  }
+];
+
+const typeDefs = `
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
+
+  type Query {
+    allUsers: [User!]
+  }
+
+  type Mutation {
+    createUser(name:String!, email:String!) : User
+  }
+
+`;
+
+const resolvers = {
+  Query: {
+    allUsers: () => users
+  },
+
+  Mutation: {
+    createUser: (parent, args) => {
+      const newUser = Object.assign({ id: users.length + 1 }, args);
+      users.push(newUser);
+      return newUser;
+    }
+  }
+};
+
+export default makeExecutableSchema({ typeDefs, resolvers });
